c1290d58c8cbe0f9a377fde075cefb7b514542f0
shmublon <shmublon@gmail.com>
2018-05-25 13:58:50 +0200

    procfile

--- routes/index.js
+++ routes/index.js
@@ -18,40 +18,41 @@ telegramBot.on("message", function (msg) {
     if (msg.text !== '/start') {
         request("https://questions-engine.herokuapp.com/random-question", function (error, response, question) {
             nextQuestion(msg, JSON.parse(question)).then(function (previousQuestion) {
-                console.log(previousQuestion);
-                if (userId === null) {
-                    request.post({
-                        url: 'https://questions-engine.herokuapp.com/create-user-if-not-exist',
-                        form: {
-                            'name': msg.from.first_name,
-                            'age': 0,
-                            'gender': 'male',
-                            'balance': 0
-                        }
-                    }, function (err, httpResponse, body) {
-                        userId = JSON.parse(body).id;
+                if (previousQuestion) {
+                    if (userId === null) {
+                        request.post({
+                            url: 'https://questions-engine.herokuapp.com/create-user-if-not-exist',
+                            form: {
+                                'name': msg.from.first_name,
+                                'age': 0,
+                                'gender': 'male',
+                                'balance': 0
+                            }
+                        }, function (err, httpResponse, body) {
+                            userId = JSON.parse(body).id;
+                            request.post({
+                                url: 'https://questions-engine.herokuapp.com/store-result',
+                                form: {
+                                    'answer': msg.text,
+                                    'user_id': userId,
+                                    'question_id': previousQuestion.id
+                                }
+                            }, function (error, response, body) {
+                                console.log('Stored answer id: ' + JSON.parse(body).id)
+                            });
+                        })
+                    } else {
                         request.post({
                             url: 'https://questions-engine.herokuapp.com/store-result',
                             form: {
                                 'answer': msg.text,
                                 'user_id': userId,
-                                'question_id': JSON.parse(question).id
+                                'question_id': previousQuestion.id
                             }
                         }, function (error, response, body) {
                             console.log('Stored answer id: ' + JSON.parse(body).id)
                         });
-                    })
-                } else {
-                    request.post({
-                        url: 'https://questions-engine.herokuapp.com/store-result',
-                        form: {
-                            'answer': msg.text,
-                            'user_id': userId,
-                            'question_id': JSON.parse(question).id
-                        }
-                    }, function (error, response, body) {
-                        console.log('Stored answer id: ' + JSON.parse(body).id)
-                    });
+                    }
                 }
             });
         });
@@ -59,18 +60,22 @@ telegramBot.on("message", function (msg) {
 });
 
 function nextQuestion(msg, question) {
-    var answers = question.answers.split("/");
-    var answersButtons = [];
-    answers.forEach(function (value) {
-        var buffer = [];
-        buffer.push(value);
-        answersButtons.push(buffer)
-    });
-    answersButtons.push(["пропустить"]);
-    return telegramBot.sendMessage(msg.chat.id, question.text.toString(), {
-        "reply_markup": {
-            "keyboard": answersButtons,
-            "one_time_keyboard": true
-        }
-    });
+    return new Promise(function (resolve, reject) {
+        var answers = question.answers.split("/");
+        var answersButtons = [];
+        answers.forEach(function (value) {
+            var buffer = [];
+            buffer.push(value);
+            answersButtons.push(buffer)
+        });
+        answersButtons.push(["пропустить"]);
+        telegramBot.sendMessage(msg.chat.id, question.text.toString(), {
+            "reply_markup": {
+                "keyboard": answersButtons,
+                "one_time_keyboard": true
+            }
+        }).then(function (value) {
+            resolve(question);
+        });
+    })
 }
