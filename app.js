341e36493e34ecf5b86ef262bb60f2fe5edb1ba6
shmublon <shmublon@gmail.com>
2018-05-25 14:27:46 +0200

    test changes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var compression = require('compression');
+var logger = require('morgan');
 
-var question = require('./routes/telegram');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -14,28 +14,28 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(bodyParser.json({limit: '5mb'}));
-app.use(bodyParser.urlencoded({ extended: false, limit: '5mb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('', question);
-app.use('', user);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
-app.use(express.static(__dirname + '../public'));
-app.set('views', __dirname + '/public');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-app.use(bodyParser.urlencoded({
-    extended: true, limit: '5mb'
-}));
-app.use(bodyParser.json({limit: '5mb'}));
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.get('/', function(req, res){
-    res.render('main.html');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
