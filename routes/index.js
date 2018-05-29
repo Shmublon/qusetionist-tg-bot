6ad21a91f746fb7acaa600b8e905fe30333cc00f
shmublon <shmublon@gmail.com>
2018-05-29 14:51:07 +0200

    small change

--- routes/index.js
+++ routes/index.js
@@ -66,12 +66,14 @@ telegramBot.on("message", function (msg) {
                                         'balance': balance
                                     }
                                 }, function (error, response, user) {
-                                    telegramBot.sendMessage(msg.chat.id,
-                                        "Вы заработали токен! Ваш баланс: <b>" + JSON.parse(user).balance + "</b>",
-                                        {
-                                            "parse_mode": "html",
-                                            "disable_notification": true
-                                        });
+                                    if (!skipped) {
+                                        telegramBot.sendMessage(msg.chat.id,
+                                            "Вы заработали токен! Ваш баланс: <b>" + JSON.parse(user).balance + "</b>",
+                                            {
+                                                "parse_mode": "html",
+                                                "disable_notification": true
+                                            });
+                                    }
                                     resolve(userId);
                                 });
                             });
